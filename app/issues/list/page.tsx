fdc639513a8401f897e13993f53feb921611828f
Victor <victorbonatoit@gmail.com>
2024-03-22 19:20:31 -0300

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -26,10 +26,17 @@ const IssuesPage = async ({
     ? searchParams.status
     : undefined;
 
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: {
       status: status,
     },
+    orderBy,
   });
 
   return (
